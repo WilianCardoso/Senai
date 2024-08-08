99c73a00a2d7d3c7aa8301f5822fc6ef118c43bf
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2024-08-07 21:49:33 -0300

    aulinha de banco de dados zikaaa

---
+++ Ténico em Desenvolvimento de Sistemas/Banco de Dados/Projeto Gestão de Garagem/GestaoDeGaragem.sql
@@ -0,0 +1,35 @@
+CREATE SCHEMA `garagem` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+CREATE TABLE `garagem`.`car` (
+  `placa` CHAR(7) NOT NULL COMMENT 'Campo armazena a placa do veículo',
+  PRIMARY KEY (`placa`),
+  UNIQUE INDEX `placa_UNIQUE` (`placa` ASC) VISIBLE)
+COMMENT = 'Essa tabela armazena dados de veículos';
+
+ALTER TABLE `garagem`.`car` 
+ADD COLUMN `fabricante` INT NOT NULL AFTER `placa`;
+
+CREATE TABLE `garagem`.`manufatory` (
+  `codigo` INT NOT NULL AUTO_INCREMENT,
+  `fabricante` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`codigo`));
+
+ALTER TABLE `garagem`.`car` 
+ADD INDEX `fabricante_fk_idx` (`fabricante` ASC);
+;
+ALTER TABLE `garagem`.`car` 
+ADD CONSTRAINT `fabricante_fk`
+  FOREIGN KEY (`fabricante`)
+  REFERENCES `garagem`.`manufatory` (`codigo`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION;
+  
+INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Honda');
+INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Chevrolet');
+INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Renault');
+INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Chery');
+INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Kia');
+INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('VW');
+INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Hyndai');
+
+INSERT INTO `garagem`.`car` (`placa`, `fabricante`) VALUES ('GFY1B77', '4');

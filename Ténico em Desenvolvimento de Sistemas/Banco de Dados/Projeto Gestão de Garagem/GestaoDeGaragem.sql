6891347ba35b75f8ea1b7aeeb87b00215693322e
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2024-08-14 20:29:58 -0300

    ficando paranoico

--- Ténico em Desenvolvimento de Sistemas/Banco de Dados/Projeto Gestão de Garagem/GestaoDeGaragem.sql
+++ Ténico em Desenvolvimento de Sistemas/Banco de Dados/Projeto Gestão de Garagem/GestaoDeGaragem.sql
@@ -1,35 +1,39 @@
 CREATE SCHEMA `garagem` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
-CREATE TABLE `garagem`.`car` (
-  `placa` CHAR(7) NOT NULL COMMENT 'Campo armazena a placa do veículo',
+CREATE TABLE `car` (
+  `placa` char(7) NOT NULL COMMENT 'Campo armazena a placa do veículo',
+  `fabricante` int(11) NOT NULL,
+  `ano` int(2) NOT NULL,
+  `cor` int(2) NOT NULL,
+  `modelo` int(2) NOT NULL,
+  `opcional` int(2) NOT NULL,
   PRIMARY KEY (`placa`),
-  UNIQUE INDEX `placa_UNIQUE` (`placa` ASC) )
-COMMENT = 'Essa tabela armazena dados de veículos';
+  UNIQUE KEY `placa_UNIQUE` (`placa`),
+  KEY `fabricante_fk` (`fabricante`),
+  KEY `fk_ano_idx` (`ano`),
+  KEY `fk_cor_idx` (`cor`),
+  KEY `fk_modelo_idx` (`modelo`),
+  CONSTRAINT `fabricante_fk` FOREIGN KEY (`fabricante`) REFERENCES `manufatory` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_ano` FOREIGN KEY (`ano`) REFERENCES `ano` (`idano`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_cor` FOREIGN KEY (`cor`) REFERENCES `cor` (`idcor`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_modelo` FOREIGN KEY (`modelo`) REFERENCES `manufatory` (`codigo`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Essa tabela armazena dados de veículos';
 
-ALTER TABLE `garagem`.`car` 
-ADD COLUMN `fabricante` INT NOT NULL AFTER `placa`;
+CREATE TABLE `ano` (
+  `idano` int(11) NOT NULL AUTO_INCREMENT,
+  `ano` int(4) NOT NULL,
+  PRIMARY KEY (`idano`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE `garagem`.`manufatory` (
-  `codigo` INT NOT NULL AUTO_INCREMENT,
-  `fabricante` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`codigo`));
+CREATE TABLE `cor` (
+  `idcor` int(11) NOT NULL AUTO_INCREMENT,
+  `cor` varchar(30) NOT NULL,
+  PRIMARY KEY (`idcor`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-ALTER TABLE `garagem`.`car` 
-ADD INDEX `fabricante_fk_idx` (`fabricante` ASC);
-;
-ALTER TABLE `garagem`.`car` 
-ADD CONSTRAINT `fabricante_fk`
-  FOREIGN KEY (`fabricante`)
-  REFERENCES `garagem`.`manufatory` (`codigo`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
-  
-INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Honda');
-INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Chevrolet');
-INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Renault');
-INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Chery');
-INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Kia');
-INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('VW');
-INSERT INTO `garagem`.`manufatory` (`fabricante`) VALUES ('Hyndai');
-
-INSERT INTO `garagem`.`car` (`placa`, `fabricante`) VALUES ('GFY1B77', '4');
+CREATE TABLE `manufatory` (
+  `codigo` int(11) NOT NULL AUTO_INCREMENT,
+  `fabricante` varchar(50) NOT NULL,
+  `modelo` varchar(45) NOT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;

bb70a4fba440224d577cdc3c549273c94752ac5c
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2024-08-19 20:12:17 -0300

    desafios java

--- Ténico em Desenvolvimento de Sistemas/Programação de Aplicativo/Java/aula01/src/App.java
+++ Ténico em Desenvolvimento de Sistemas/Programação de Aplicativo/Java/aula01/src/App.java
@@ -1,5 +1,25 @@
+import java.time.LocalDate;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner ler = new Scanner(System.in);
+
+        Double imc;
+        Double peso = 115.5;
+        Double altura = 1.94;
+
+        System.out.print("Entre com o valor do seu peso:");
+        peso = ler.nextDouble();
+        
+        System.out.print("Entre com o valor da sua altura:");
+        altura = ler.nextDouble();
+
+        imc = peso / (altura * altura);
+        System.out.printf("%.2f%n",imc);
+
     }
 }

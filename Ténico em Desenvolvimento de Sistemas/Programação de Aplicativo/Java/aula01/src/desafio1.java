df055ccab7aa9ecb8a1a3aa290bf06064fbd8ef7
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2024-08-21 20:39:17 -0300

    refatorando codigo

--- Ténico em Desenvolvimento de Sistemas/Programação de Aplicativo/Java/aula01/src/desafio1.java
+++ Ténico em Desenvolvimento de Sistemas/Programação de Aplicativo/Java/aula01/src/desafio1.java
@@ -1,42 +1,49 @@
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class desafio1 {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         Locale.setDefault(Locale.US);
 
-        Double a, b, c, p, area1, area2;
+        //Double a, b, c, p, 
+        double area1, area2;
 
-        System.out.print("Entre com o primeiro valor:");
-        a = read.nextDouble();
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+        
 
-        System.out.print("Entre com o segundo valor:");
-        b = read.nextDouble();
+        System.out.print("Entre com os valores do triangulo X:");
+        x.a = read.nextDouble();
 
-        System.out.print("Entre com o terceiro valor:");
-        c = read.nextDouble();
+        x.b = read.nextDouble();
 
-        p = (a + b + c) / 2;
+        x.c = read.nextDouble();
 
-        area1 = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-        System.out.println("A área do trianguloX é: %.2f\n" + area1);
+        area1 = x.area();
 
-        System.out.print("Entre com o primeiro valor:");
-        a = read.nextDouble();
+        System.out.print("Entre com os valores do triangulo Y:");
 
-        System.out.print("Entre com o segundo valor:");
-        b = read.nextDouble();
+        y.a = read.nextDouble();
 
-        System.out.print("Entre com o terceiro valor:");
-        c = read.nextDouble();
+        y.b = read.nextDouble();
 
-        p = (a + b + c) / 2;
+        y.c = read.nextDouble();
 
-        area2 = Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        area2 = y.area();
 
-        System.out.println("A área do trianguloY é: %.2f\n" + area2);
+        System.out.printf("A area do trianguloX e: %.4f%n", area1);
+        System.out.printf("A area do trianguloY e: %.4f%n", area2);
 
+        if(area1 > area2){
+            System.out.println("TrianguloX e maior");
+        }else{
+            System.out.println("TrianguloY e maior");
+        }
     }
+    
 
 }

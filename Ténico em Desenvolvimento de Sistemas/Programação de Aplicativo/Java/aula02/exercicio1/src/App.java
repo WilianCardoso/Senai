8a76657d7884e94c90ad0de5d2b9eb1d468ce62c
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2024-08-25 23:00:48 -0300

    exercicios

--- Ténico em Desenvolvimento de Sistemas/Programação de Aplicativo/Java/aula02/exercicio1/src/App.java
+++ Ténico em Desenvolvimento de Sistemas/Programação de Aplicativo/Java/aula02/exercicio1/src/App.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+import java.util.Locale;
+import entities.rectangle;
+
 public class App {
+    
     public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        rectangle r = new rectangle();
+        Scanner read = new Scanner(System.in);
+
+        System.out.println("Entre com a altura e largura do Retangulo:");
+        r.height = read.nextDouble();
+        r.width = read.nextDouble();
+
+        System.out.print("Area: "+ r.area()+"\n");
+        System.out.print("Perimeter: "+r.perimeter()+ "\n");
+        System.out.print("Diagonal: "+r.diagonal());
         
+
     }
 }

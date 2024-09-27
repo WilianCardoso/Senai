2780a40fedaa0add5dae3f46a0b78a0695301511
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2024-09-26 21:44:11 -0300

    aprendendo novos comandos no SQL

--- Ténico em Desenvolvimento de Sistemas/Banco de Dados/introdução sql -BD.sql
+++ Ténico em Desenvolvimento de Sistemas/Banco de Dados/introdução sql -BD.sql
@@ -1,45 +1,88 @@
-create table empregado(
-id_empregado int,
-nm_empregado varchar (45),
-funcao varchar(45),
-data_contratacao date,
-salario decimal (10,2),
-comissao decimal (10,2),
-id_departamento int,
-lider int
-);
 
 create table grade_salarial(
-menor_salario decimal (10,2),
-maior_salario decimal (10,2)
+menor_salario decimal (10,2) not null,
+maior_salario decimal (10,2) not null,
+constraint grade_salarial_id_grade_salarial_pk
+primary key (id_grade_salarial)
 );
 
 create table departamento(
 id_departamento int,
-nm_departamento varchar (30),
-localizacao varchar(20)
+nm_departamento varchar (30) not null,
+localizacao varchar(20) not null,
+constraint id_departamento_pk
+primary key (id_departamento)
 );
 
-create table departamento30
-As
-	select id_empregado, nm_empregado, salario,
-    data_contratacao
-    from empregado
-    where id_departamento = 30
-;
 
-drop table departamento30;
+create table empregado(
+id_empregado int,
+nm_empregado varchar (45) not null,
+funcao varchar(45) not null,
+data_contratacao date not null,
+salario decimal (10,2) not null,
+comissao decimal (10,2),
+id_departamento int not null,
+lider int,
+constraint empregado_id_empregado_pk
+primary key empregado(id_empregado),
+constraint empregado_lider_fk
+foreign key (lider) references empregado(id_empregado),
+constraint empregado_id_empregado_fk
+foreign key (id_departamento) references departamento(id_departamento)
+);
 
-alter table departamento30
-	add tarefa varchar(9);
+#preencher a tabela - emxemplo com dois campos inseridos
+insert into departamento (id_departamento, nm_departamento, localizacao)
+value (50, 'Desenvolvimento', 'Detroide'); 
+
+insert into departamento value(60, 'Web-design', 'Timbo');
+ 
+insert into departamento value(70, 'Suporte', 'Detroit');
+
+#exemplo sem passar os parametros
+insert into departamento value (80, 'HR', 'IndaHell');
+
+insert into empregado values (1, "Hermano","Professor", "2014-03-02", '1000', null, 60, null);
+insert into empregado values (2, "Gerson","Professor", "2019-05-01", '1500', null, 60, null);
+insert into empregado values (3, "Gustavo","Professor", "2021-10-01", '1500', null, 70, 1);
+
+insert into empregado (id_empregado, nm_empregado, funcao, data_contratacao, salario, comissao, id_departamento) values (4, "Sabrina","Atentimento", "2023-12-01", '400', 100, 60);
+
+update empregado set nm_empregado = "Hermano Roepke" where id_empregado = 1;
+#visualisar a tabela, * - tudo!!!
+select * from empregado;
     
-alter table departamento30
-	modify tarefa varchar(15);
-    
-alter table departamento30
-	drop tarefa;
-    
-desc departamento30;
 desc empregado;
 desc departamento;
 desc grade_salarial;
+
+alter table empregado
+	add constraint empregado_id_empregado_pk
+    primary key(id_empregado);
+    
+    #Alterar chave primaria de empregado
+alter table empregado add constraint empregado_id_empregado_pk
+primary key(id_empregado);
+    
+    #add chave primaria no departamento
+alter table departamento
+		add	constraint departamento_id_departamento_pk
+        primary key (id_departamento);
+        
+    #add chave estrangeira no empregado
+alter table empregado
+		add constraint empregado
+        foreign key (id_departamento)
+        references departamento(id_departamento);
+        
+     #colocar chave estrangeira como not null
+ alter table empregado
+	modify column lider int not null;
+    
+    #add chave estrangeira no empregado
+    alter table empregado
+		add constraint lider
+        foreign key (lider)
+        references empregado(id_empregado);
+        

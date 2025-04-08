47b61b9d3483195a7d5f11617e8067d06a479291
WilianCardoso <165498161+WilianCardoso@users.noreply.github.com>
2025-04-07 21:43:46 -0300

    inicio da base para o projeto da bancada

---
+++ Ténico em Desenvolvimento de Sistemas/Desenvolvimento de Sistemas/clpmonitor/src/main/java/com/clpmonitor/clpmonitor/ClpmonitorApplication.java
@@ -0,0 +1,13 @@
+package com.clpmonitor.clpmonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClpmonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClpmonitorApplication.class, args);
+	}
+
+}
---
+++ Ténico em Desenvolvimento de Sistemas/Desenvolvimento de Sistemas/clpmonitor/src/test/java/com/clpmonitor/clpmonitor/ClpmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clpmonitor.clpmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClpmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
